28b36fa5125c9b78cec8d594b0a86826c35f168d
CeliaBillaud <celiabillaud2@gmail.com>
2025-01-07 16:43:49 +0100

    RecoList component created

--- assets/react/controllers/RecoList.jsx
+++ assets/react/controllers/RecoList.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import Reco from "./Reco";
 
-export default function RecoList(props) {
+export default function RecoList({recos}) {
     return <div className="flex flex-col gap-4">
+        {recos.map((reco, index) => (
+            <Reco
+                key={index}
+                image={reco.image}
+                imageAlt={reco.imageAlt}
+                userName={reco.userName}
+                title={reco.title}
+                content={reco.content}
+            />
+        ))
 
+        }
     </div>
 }
