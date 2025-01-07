cea860a2a2d06ad54449bccd574e5e2908cae582
CeliaBillaud <celiabillaud2@gmail.com>
2025-01-07 12:15:01 +0100

    Reco component done

--- assets/react/controllers/Reco.jsx
+++ assets/react/controllers/Reco.jsx
@@ -1,32 +1,41 @@
 import React from 'react';
 
 export default function (props) {
-    return <div className="card lg:card-side bg-base-100 shadow-xl p-6">
-        {/* if user has a profil pic */}
-                {/* <div className="avatar">
-                    <div className="w-24 rounded-full">
-                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp" />
-                    </div>
-                </div> */}
-                {/* <div className="avatar placeholder">
-                    <div className="bg-neutral text-neutral-content w-24 rounded-full">
-                        <span className="text-3xl">D</span>
-                    </div>
-                </div> */}
-                <div className="card-body">
-                    <h2 className="card-title">{props.title}</h2>
-                    <p>{props.content}</p>
+    return <article className="flex gap-4 items-center w-full bg-white py-6 px-6 border border-gray-200 rounded-md">
+                <div className="h-48 w-72 relative flex items-center object-contain rounded-xl overflow-hidden">
+                            <img
+                                src={props.image}
+                                alt={props.imageAlt}
+                             />
                 </div>
-                <figure className="px-10 pt-10">
-                <img
-                    src={props.image}
-                    alt={props.imageAlt}
-                    className="rounded-xl" />
-                </figure>
+            <div className='flex flex-col'>
+                <div className="flex items-center gap-x-1 text-xs">
+                    <a className="relative z-30 group" href="/">
+                        <span className="flex items-center justify-center gap-2">
+                            <div className="text-white h-8 w-8 rounded-full ring-2 ring-white flex items-center justify-center font-bold bg-black">J</div>
+                        <span className="group-hover:text-violet-700">{props.userName}</span>
+                        <time className="text-gray-500 text-xs">December 19, 2024</time>
+                        </span>
+                    </a>
+                </div>
 
+                <div className="flex justify-between items-center w-full">
+                    <h3 className="mt-1 text-lg font-semibold leading-6 text-gray-900 group-hover:text-gray-600">
+                        <span className="text-2xl font-bold">{props.title}</span>
+                    </h3>
+                </div>
+                
+                 <p className='mt-2 line-clamp-3 text-sm leading-6 text-gray-600'>{props.content}</p>
+                 </div>
+                
+            </article>    
 
-                <div></div>
-            </div>
+                    
+                            
+                                    
+                                    
+                                                                
+            
 
 
            
