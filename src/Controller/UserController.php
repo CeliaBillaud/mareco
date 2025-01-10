a6912b2d4b0ea938e53d46624d1323039f33fe04
CeliaBillaud <celiabillaud2@gmail.com>
2025-01-10 12:12:03 +0100

    navbar changed depending on login or logout

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -65,7 +65,7 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route(path: '/deconnexion', name: 'app_user_deconnexion')]
+    #[Route(path: '/deconnexion', name: 'app_user_logout')]
     public function logout(): void
     {
         throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');}
